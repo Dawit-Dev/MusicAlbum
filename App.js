e52b2da7d7633d7569ef65c73715c5b39d4475fb
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-06-26 18:29:14 +0100

    Consumint File Exports

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header from './src/components/Header';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
     </View>
   );
 }
@@ -18,3 +18,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
