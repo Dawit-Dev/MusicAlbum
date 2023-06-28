7a8ad93a0025257f48acded6bc78fd704b977bcc
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-06-28 21:09:48 +0100

    Displaying Album Artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,18 +1,60 @@
-import React from 'react'
-import { StyleSheet, Text} from 'react-native'
-import Card from '../cards/Card'
-import CardSection from '../cards/CardSection'
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Card from "../cards/Card";
+import CardSection from "../cards/CardSection";
 
-const AlbumDetail = ({ album}) => {
+const AlbumDetail = ({ album }) => {
+  const {
+    imageStyle,
+    headerContentStle,
+    imageContainerStyle,
+    headerTextStyle,
+    coverImageStyle,
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <Text>{album.title}</Text>
+        <View style={imageContainerStyle}>
+          <Image
+            style={imageStyle}
+            source={{ uri: album.artist.picture_medium }}
+          />
+        </View>
+        <View style={headerContentStle}>
+          <Text style={headerTextStyle}>{album.title}</Text>
+          <Text>{album.artist.name}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={coverImageStyle} source={{ uri: album.cover_medium }} />
       </CardSection>
     </Card>
   );
-}
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headerContentStle: {
+    flexDirection: "column",
+    justifyContent: "space-around",
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  imageStyle: {
+    height: 50,
+    width: 50,
+  },
+  imageContainerStyle: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  coverImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+});
 
-export default AlbumDetail
+export default AlbumDetail;
